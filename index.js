37a6a2e3d08e01f802a30c5a10d59375e501ce65
kasai441 <kasai441441@gmail.com>
2021-05-12 11:22:51 +0900

    引数から値を受け取って表示

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 const wiktionary = require('wiktionary-node')
+const argv = require('minimist')(process.argv)
+const sqlite3 = require('sqlite3').verbose()
 
 const words = ['Sunday', 'jump', 'magazine', 'champion', 'king', 'ribbon', 'ciao']
 
-async function run () {
-  for (i = 0; i < words.length; i++) {
-    const result = await wiktionary(words[i]) 
+class WordQuiz {
+  static async run () {
+    let word = null
+    if (argv._.length === 3) {
+      word = argv._[2]
+    } else {
+      throw new Error('不正な引数')
+    }
+    const result = await wiktionary(word) 
     if (result === null) {
-      console.log(`${words[i]} is not on Wictionary`)
+      console.log(`${word} is not on Wictionary`)
     } else {
       console.log(result.word)
       result.definitions.forEach(definition => {
         console.log('- ' + definition.speech)
         const lines = definition.lines
-        for (j = 0; j < 3 && j < lines.length; j++) {
+        for (let j = 0; j < 3 && j < lines.length; j++) {
           const line = lines[j]
           console.log((j + 1) + '. ' + line.define)
           if (line.examples.length > 0) console.log("'" + line.examples[0] + "'")        
         }
       })
-    }
-  }  
+      console.log('登録しますか')
+    }  
+  }
 }
 
-run()
+WordQuiz.run()
