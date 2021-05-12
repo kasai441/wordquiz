5153726d1ed298cf5450cf116beac6aecdd496e0
kasai441 <kasai441441@gmail.com>
2021-05-12 11:58:40 +0900

    Wordクラス、Storageクラス雛形

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const wiktionary = require('wiktionary-node')
 const argv = require('minimist')(process.argv)
 const sqlite3 = require('sqlite3').verbose()
 
+const currentDir = process.cwd()
+const databaseName = `${currentDir}/wordquiz.sqlite3`
+const tableName = 'words'
+
 const words = ['Sunday', 'jump', 'magazine', 'champion', 'king', 'ribbon', 'ciao']
 
 class WordQuiz {
@@ -12,22 +16,59 @@ class WordQuiz {
     } else {
       throw new Error('不正な引数')
     }
-    const result = await wiktionary(word) 
-    if (result === null) {
-      console.log(`${word} is not on Wictionary`)
+    const word_data = await wiktionary(word).catch(() => null)
+    if (word_data === null || word_data.definitions.length === 0) {
+      console.log(`${word} can't receive any correct definitions from Wictionary`)
     } else {
-      console.log(result.word)
-      result.definitions.forEach(definition => {
-        console.log('- ' + definition.speech)
-        const lines = definition.lines
-        for (let j = 0; j < 3 && j < lines.length; j++) {
-          const line = lines[j]
-          console.log((j + 1) + '. ' + line.define)
-          if (line.examples.length > 0) console.log("'" + line.examples[0] + "'")        
-        }
-      })
-      console.log('登録しますか')
+      this.register(word_data)
     }  
+  }
+
+  register (word_data) {
+    console.log(word_data.word)
+
+    Storage.insert(word_data)
+    word_data.definitions.forEach(definition => {
+      console.log('- ' + definition.speech)
+      const lines = definition.lines
+      for (let j = 0; j < 3 && j < lines.length; j++) {
+        const line = lines[j]
+        console.log((j + 1) + '. ' + line.define)
+        if (line.examples.length > 0) console.log("'" + line.examples[0] + "'")        
+      }
+    })
+    console.log('登録しました')
+  }
+}
+
+class Word {
+  constructor (name, definitions) {
+    this.name = name
+    this.definitions = definitions
+  }
+}
+
+class Storage {
+  static connect () {
+    const db = new sqlite3.Database(databaseName)
+    db.run(`CREATE TABLE IF NOT EXISTS ${tableName} (content TEXT)`)
+    return db
+  }
+
+  static insert (content) {
+    const query = `INSERT INTO ${tableName} VALUES (?)`
+    const db = this.connect()
+    try {
+      db.serialize(function () {
+        const stmt = db.prepare(query)
+        stmt.run(content)
+        stmt.finalize()
+      })
+    } catch (e) {
+      return e
+    } finally {
+      db.close()
+    }
   }
 }
 
