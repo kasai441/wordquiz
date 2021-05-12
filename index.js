522341334f8dd9d30820ba7d4d31b7195ba0ac8d
kasai441 <kasai441441@gmail.com>
2021-05-12 14:54:45 +0900

    DBインサート雛形

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 const wiktionary = require('wiktionary-node')
+// eslint-disable-next-line no-undef
 const argv = require('minimist')(process.argv)
 const sqlite3 = require('sqlite3').verbose()
 
+// eslint-disable-next-line no-undef
 const currentDir = process.cwd()
 const databaseName = `${currentDir}/wordquiz.sqlite3`
 const tableName = 'words'
 
-const words = ['Sunday', 'jump', 'magazine', 'champion', 'king', 'ribbon', 'ciao']
+// const words = ['Sunday', 'jump', 'magazine', 'champion', 'king', 'ribbon', 'ciao']
 
 class WordQuiz {
   static async run () {
-    let word = null
+    let input_word = null
     if (argv._.length === 3) {
-      word = argv._[2]
+      input_word = argv._[2]
     } else {
       throw new Error('不正な引数')
     }
-    const word_data = await wiktionary(word).catch(() => null)
-    if (word_data === null || word_data.definitions.length === 0) {
-      console.log(`${word} can't receive any correct definitions from Wictionary`)
+    const result = await wiktionary(input_word).catch(() => null)
+    if (result === null || result.definitions.length === 0) {
+      console.log(`${input_word} can't receive any correct definitions from Wictionary`)
     } else {
-      this.register(word_data)
+      this.register(new Word(result.name, result.definitions))
     }  
   }
 
-  register (word_data) {
-    console.log(word_data.word)
+  static register (word) {
+    // console.log(word_data.word)
 
-    Storage.insert(word_data)
-    word_data.definitions.forEach(definition => {
+    Storage.insert(word)
+    word.definitions.forEach(definition => {
       console.log('- ' + definition.speech)
       const lines = definition.lines
       for (let j = 0; j < 3 && j < lines.length; j++) {
@@ -45,23 +47,42 @@ class Word {
   constructor (name, definitions) {
     this.name = name
     this.definitions = definitions
+    this.updatedAt = new Date()
+    this.createdAt = new Date()
   }
 }
 
 class Storage {
   static connect () {
     const db = new sqlite3.Database(databaseName)
-    db.run(`CREATE TABLE IF NOT EXISTS ${tableName} (content TEXT)`)
+    db.run(`
+      CREATE TABLE IF NOT EXISTS ${tableName} (
+        name TEXT primary key,
+        definitions TEXT,
+        updated_at NUMERIC,
+        created_at NUMERIC        
+      )
+    `)
     return db
   }
 
-  static insert (content) {
-    const query = `INSERT INTO ${tableName} VALUES (?)`
+  static insert (word) {
+    const query = `INSERT INTO ${tableName} (
+        name,
+        definitions,
+        updated_at,
+        created_at
+      ) VALUES (?, ?, ?, ?)`
     const db = this.connect()
     try {
       db.serialize(function () {
         const stmt = db.prepare(query)
-        stmt.run(content)
+        stmt.run(
+          word.name,
+          word.definitions,
+          word.updatedAt,
+          word.createdAt
+          )
         stmt.finalize()
       })
     } catch (e) {
